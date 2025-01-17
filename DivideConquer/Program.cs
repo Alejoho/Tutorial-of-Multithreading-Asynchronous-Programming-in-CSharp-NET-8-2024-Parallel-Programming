e3b6d68f9dae754f98b65e1b3f713f917641c97c
Alejoho <alejoreilly96@gmail.com>
2025-01-16 20:00:32 -0500

    Add the 2nd example

---
+++ DivideConquer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+int sum = 0;
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+foreach (var num in array)
+{
+    Thread.Sleep(100);
+    sum += num;
+}
+
+stopwatch.Stop();
+
+Console.WriteLine($"The sum is {sum}");
+Console.WriteLine($"The time it takes: {stopwatch.Elapsed.TotalMilliseconds}");
+
+Console.ReadLine();
