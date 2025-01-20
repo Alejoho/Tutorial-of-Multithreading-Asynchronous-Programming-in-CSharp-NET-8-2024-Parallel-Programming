1a66dcdbb5891a6f00a0172743ecefc33816d456
Alejoho <alejoreilly96@gmail.com>
2025-01-20 11:03:25 -0500

    Thread Sync overview. Synchronously

---
+++ SynchronizationOverview/Program.cs
@@ -0,0 +1,19 @@
+﻿int counter = 0;
+
+Thread thread1 = new Thread(IncrementCounter);
+thread1.Start();
+thread1.Join();
+
+Thread thread2 = new Thread(IncrementCounter);
+thread2.Start();
+thread2.Join();
+
+Console.WriteLine($"Final counter value is: {counter}");
+
+void IncrementCounter()
+{
+    for (int i = 0; i < 100000; i++)
+    {
+        counter = counter + 1;
+    }
+}
