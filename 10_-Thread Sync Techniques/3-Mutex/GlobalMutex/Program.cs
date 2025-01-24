d8cf19bdfcc6abfb07b883b7b95960dc6714a443
Alejoho <alejoreilly96@gmail.com>
2025-01-24 12:15:13 -0500

    Complete Mutex sync technique

---
+++ 10_-Thread Sync Techniques/3-Mutex/GlobalMutex/Program.cs
@@ -0,0 +1,30 @@
+﻿using GlobalMutex;
+
+string filePath = "counter.txt";
+
+//File.Delete(filePath);
+
+using (var mutex = new Mutex(false, filePath))
+{
+    mutex.WaitOne();
+    try
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            int counter = HelperFunctions.ReadCounter(filePath);
+            counter++;
+            HelperFunctions.WriteCounter(filePath, counter);
+        }
+    }
+    finally
+    {
+        mutex.ReleaseMutex();
+    }
+}
+
+Console.WriteLine("Process finished.");
+
+//string content = File.ReadAllText(filePath);
+//Console.WriteLine($"File content: {content}");
+
+Console.ReadLine();
---
+++ 10_-Thread Sync Techniques/3-Mutex/LocalMutex/Program.cs
@@ -0,0 +1,35 @@
+﻿int counter = 0;
+
+Mutex mutex = new Mutex();
+
+Thread thread1 = new Thread(IncrementCounter);
+Thread thread2 = new Thread(IncrementCounter);
+Thread thread3 = new Thread(IncrementCounter);
+
+thread1.Start();
+thread2.Start();
+thread3.Start();
+
+thread1.Join();
+thread2.Join();
+thread3.Join();
+
+Console.WriteLine($"Final counter value is: {counter}");
+Console.ReadLine();
+
+void IncrementCounter()
+{
+    for (int i = 0; i < 20000; i++)
+    {
+        mutex.WaitOne();
+        try
+        {
+            int temp = counter;
+            counter = temp + 1;
+        }
+        finally
+        {
+            mutex.ReleaseMutex();
+        }
+    }
+}
