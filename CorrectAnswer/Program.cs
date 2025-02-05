01c370b3f2b9615f6c30f628fd45b08a986cf707
Alejoho <alejoreilly96@gmail.com>
2025-02-05 18:06:33 -0500

    Solve the 3rd question

---
+++ CorrectAnswer/Program.cs
@@ -0,0 +1,62 @@
+﻿Queue<int> queue = new Queue<int>();
+
+int threadsCompleted = 0;
+
+object countLock = new object();
+
+using AutoResetEvent producerEvent = new AutoResetEvent(true);
+using SemaphoreSlim consumerSemaphore = new SemaphoreSlim(0, 3);
+
+for (int i = 1; i <= 3; i++)
+{
+    Thread thread = new Thread(Consume);
+    thread.Name = $"Consumer {i}";
+    thread.Start();
+}
+
+// Producer
+while (true)
+{
+    producerEvent.WaitOne();
+
+    Console.WriteLine("Press 'p' to produce.");
+
+    if (Console.ReadLine() == "p")
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            queue.Enqueue(i);
+            Console.WriteLine($"Produced: {i}");
+        }
+
+        consumerSemaphore.Release(3);
+    }
+}
+
+// Consumer
+void Consume()
+{
+    while (true)
+    {
+        consumerSemaphore.Wait();
+
+        while (queue.TryDequeue(out int item))
+        {
+            Console.WriteLine($"Consumed: {item} from thread: {Thread.CurrentThread.Name}");
+            Thread.Sleep(500);
+        }
+
+        lock (countLock)
+        {
+            threadsCompleted++;
+            if (threadsCompleted == 3)
+            {
+                threadsCompleted = 0;
+                producerEvent.Set();
+                Console.WriteLine("******************************");
+                Console.WriteLine("******** More Please! ********");
+                Console.WriteLine("******************************");
+            }
+        }
+    }
+}
---
+++ TwoWaySignalingInProducer_ConsumerScenarioQuestion/Program.cs
@@ -0,0 +1,62 @@
+﻿Queue<int> queue = new Queue<int>();
+
+object lockObj = new object();
+
+const int count = 3;
+
+AutoResetEvent autoResetEvent = new AutoResetEvent(true);
+ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+
+for (int i = 1; i <= count; i++)
+{
+    Thread thread = new Thread(ProcessQueue);
+    thread.Name = $"T-{i}";
+    thread.Start();
+}
+
+while (true)
+{
+    autoResetEvent.WaitOne();
+
+    Console.WriteLine("Main thread start creating items.");
+
+    for (int i = 1; i <= count; i++)
+    {
+        queue.Enqueue(i);
+        Console.WriteLine($"{queue.Count} items in the queue.");
+    }
+
+    Console.WriteLine($"Write 'p' to start processing.");
+/*
+    while (Console.ReadLine() != signal)
+    {
+        Console.WriteLine($"Please, write {signal} to start processing.");
+    }
+*/
+    Thread.Sleep(30);
+
+    manualResetEvent.Set();
+    //manualResetEvent.Reset();
+}
+
+// If I don't put a time sleep, sometimes the threads are so fast that one
+// can actually make a dequeue over the queue when this is already empty
+
+void ProcessQueue()
+{
+    while (true)
+    {
+        manualResetEvent.WaitOne();
+
+        if (queue.TryDequeue(out int item))
+            Console.WriteLine($"Item {item} processed");
+
+        Thread.Sleep(200);
+
+        if (queue.Count == 0)
+        {
+            manualResetEvent.Reset();
+            autoResetEvent.Set();
+        }
+    }
+}
