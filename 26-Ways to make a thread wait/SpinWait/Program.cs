d3fd33da1c3d4719613b0c433d99653ee1492d58
Alejoho <alejoreilly96@gmail.com>
2025-02-15 10:32:54 -0500

    Add 2 lessons

---
+++ 26-Ways to make a thread wait/SpinWait/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Start");
+
+/*var thread1 = new Thread(Work) { Name = "Thread 1" };
+thread1.Start();*/
+
+for (var i = 0; i < 10; i++)
+{
+    var thread = new Thread(Work) { Name = $"Thread {i}" };
+    thread.Start();
+}
+
+Thread.CurrentThread.Name = "Master thread";
+Work();
+
+void Work()
+{
+    Console.WriteLine($"{Thread.CurrentThread.Name} is starting to work.");
+    Thread.SpinWait(99999999); // This number has eights 9
+    Console.WriteLine($"{Thread.CurrentThread.Name} is finishing to work.");
+}
---
+++ 26-Ways to make a thread wait/WaitUntil/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Start");
+
+var number = 0;
+Console.WriteLine($"number = {number}");
+
+var thread = new Thread(Work) { Name = "Thread 1" };
+thread.Start();
+
+thread.Join();
+Console.WriteLine($"number = {number}");
+Console.WriteLine("End");
+
+void Work()
+{
+    Console.WriteLine($"{Thread.CurrentThread.Name} is starting to work.");
+    SpinWait.SpinUntil(
+        () => ++number > 1000
+        , TimeSpan.FromMilliseconds(200)
+    );
+    Console.WriteLine($"{Thread.CurrentThread.Name} is finishing to work.");
+}
