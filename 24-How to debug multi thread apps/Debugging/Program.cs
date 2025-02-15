e03233cb8074dd0393be0d19026379072048223c
Alejoho <alejoreilly96@gmail.com>
2025-02-15 10:27:43 -0500

    New Section. How to debug multi-thread apps

---
+++ 24-How to debug multi thread apps/Debugging/Program.cs
@@ -0,0 +1,25 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Thread thread = new Thread(Work);
+    thread.Name = $"Thread {i}";
+    if (i % 2 == 0)
+    {
+        thread.Priority = ThreadPriority.Highest;
+    }
+
+    if(i==4)
+    {
+        thread.IsBackground = true;
+    }
+    thread.Start();
+}
+
+Thread.CurrentThread.Name = "Master Thread";
+Work();
+
+void Work()
+{
+    Console.WriteLine($"{Thread.CurrentThread.Name} started working");
+    Thread.Sleep(10000);
+    Console.WriteLine($"{Thread.CurrentThread.Name} finished working");
+}
