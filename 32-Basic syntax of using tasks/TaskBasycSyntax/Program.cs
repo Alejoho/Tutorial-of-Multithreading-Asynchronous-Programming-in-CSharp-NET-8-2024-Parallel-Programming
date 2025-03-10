3748383eae7f176f3f862aa951a2e5686fc47d66
Alejoho <alejoreilly96@gmail.com>
2025-03-10 11:50:39 -0400

    Add the first lessons from tasks

---
+++ 32-Basic syntax of using tasks/TaskBasycSyntax/Program.cs
@@ -0,0 +1,17 @@
+﻿// Thread thread = new Thread(Work);
+// thread.Start();
+
+// Task task = new Task(Work);
+// task.Start();
+
+Task.Run(Work);
+
+Console.ReadLine();
+
+void Work()
+{
+    Console.WriteLine("Working");
+    //Thread.Sleep(3000);
+    Console.WriteLine($"IsBackground: {Thread.CurrentThread.IsBackground}");
+    Console.WriteLine($"IsThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}");
+}
---
+++ 35-Returning results from a task/ReturningResultsFromATask/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//var t = Task.Run(() => Work(5));
+
+Task<int> t = new Task<int>(() => Work(5));
+t.Start();
+
+Console.WriteLine("Main thread");
+
+Console.WriteLine($"The result from the work is {t.Result}");
+
+Console.WriteLine("Result returned");
+
+Console.ReadLine();
+
+
+int Work(int iterations)
+{
+    Console.WriteLine("Work started");
+
+    Thread.Sleep(5000);
+    int output = 0;
+
+    for (int i = 0; i < iterations; i++)
+    {
+        output += i;
+    }
+
+    return output;
+}
