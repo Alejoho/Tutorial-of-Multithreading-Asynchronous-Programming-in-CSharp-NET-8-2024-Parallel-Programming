ac9bedc4f21f5538e633af8406acb98095572808
Alejoho <alejoreilly96@gmail.com>
2025-04-05 16:51:03 -0400

    Basic syntax async and await lessons

---
+++ 46-Basic syntax of Async and Await/AsyncAndAwaitBasicSyntax1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AsyncAndAwaitBasicSyntax1;
+
+class Program
+{
+    // Old way
+    // static void Main(string[] args)
+    // {
+    //     Console.WriteLine("Start");
+    //     Task.Run(Work).Wait();
+    //     Console.WriteLine("End");
+    // }
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Start");
+        await WorkAsync();
+        Console.WriteLine("End");
+    }
+
+    // Old way
+    // static void Work()
+    // {
+    //     Console.WriteLine("Starting to work");
+    //     Task.Delay(3000).Wait();
+    //     Console.WriteLine("Finishing work");
+    // }
+
+    static async Task WorkAsync()
+    {
+        Console.WriteLine("Starting to work");
+        await Task.Delay(4000);
+        Console.WriteLine("Finishing work");
+    }
+}
